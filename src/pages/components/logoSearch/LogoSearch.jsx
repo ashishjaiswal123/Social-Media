9bbbad0f23191f8814a8b878a23b2c0c388d2ae2
ashishjaiswal123 <jaiswalashish025@gmail.com>
2022-09-10 19:00:50 +0530

    reuired things and structured are made

---
+++ src/pages/components/logoSearch/LogoSearch.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './LogoSearch.css'
+
+const LogoSearch = () => {
+  return (
+    <div>LogoSearch</div>
+  )
+}
+
+export default LogoSearch
---
+++ src/pages/components/profileSide/ProfileSide.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import LogoSearch from '../logoSearch/LogoSearch'
+import './ProfileSide.css'
+
+const ProfileSide = () => {
+  return (
+    <div className="profileSide">
+        <LogoSearch/>
+    </div>
+  )
+}
+
+export default ProfileSide
